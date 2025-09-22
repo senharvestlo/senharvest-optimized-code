bf5eb396a58929bc0a56d83bf39137741f61afcc
SenHarvest Group <senharvestlo@gmail.com>
2025-09-22 08:59:52 -0400

    feat(pdf-html): robust pagination + i18n; add pages/Admin.jsx rendering AdminTermsPDF

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminTermsPDF from "../components/AdminTermsPDF.jsx";
+
+export default function Admin() {
+  return (
+    <div className="p-6">
+      <AdminTermsPDF />
+    </div>
+  );
+}
+
+
